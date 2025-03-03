bb90807c9f7c7e6b81fa0208bbd735fdc31523d8
fluke <chaikhwan32@gmail.com>
2025-03-03 23:36:24 +0700

    finish assignment(callback high function)

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,19 @@
 
 function forEach(array, operation) {
   // Start coding here
+    let result = []
+      for(let i = 0 ; i < array.length ; i ++){
+        result.push(operation(array[i]))
+      }
+      return result
+}
+function increaseSalary(salary){
+  return salary + 5000
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
-
+   newEmployeeSalaries.push(forEach(employeeSalaries,increaseSalary))
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -2,7 +2,25 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+    let passStudent = 0
+      for(let i = 0 ; i < array.length ; i++){
+          passStudent = passStudent + operation(array[i])
+      }
+        if(passStudent >= 5){
+          return "ผ่านเกณฑ์ ✅"
+        }
+          else{
+            return "ไม่ผ่านเกณฑ์ ❌"
+          }
 }
+function scoreUpto70(studentScore){
+    if(studentScore >= 70){
+      return 1
+    }
+    else{
+      return 0
+    }
+  }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
@@ -10,6 +28,10 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = `นักเรียนห้องที่ 1 ${atLeastFive(studentScoresRoom1,scoreUpto70)}`
+let scoreRoom2Result = `นักเรียนห้องที่ 2 ${atLeastFive(studentScoresRoom2,scoreUpto70)}`
+let scoreRoom3Result = `นักเรียนห้องที่ 3 ${atLeastFive(studentScoresRoom3,scoreUpto70)}`
+
+console.log(scoreRoom1Result)
+console.log(scoreRoom2Result)
+console.log(scoreRoom3Result)
